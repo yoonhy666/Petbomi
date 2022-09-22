58ddaac4c07b08c1254f6e61be389a91f933f29a
6502 <pih1612@naver.com>
2022-09-22 12:10:42 +0900

    회원가입창 수정

--- app/src/main/java/com/example/petbomi/RegisterActivity.java
+++ app/src/main/java/com/example/petbomi/RegisterActivity.java
@@ -1,17 +1,23 @@
 package com.example.petbomi;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
 
 public class RegisterActivity extends AppCompatActivity {
     private Spinner spinner;
+    private EditText mEtAddress;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +25,27 @@ public class RegisterActivity extends AppCompatActivity {
 
         spinner = (Spinner)findViewById(R.id.gender);
 
-
-
-
-
-
+        mEtAddress=findViewById(R.id.et_address);
+        mEtAddress.setFocusable(false);
+        mEtAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =new Intent(RegisterActivity.this,SearchActivity.class);
+                getSearchResult.launch(intent);
+            }
+        });
 
     }
 
-
+    private final ActivityResultLauncher<Intent> getSearchResult=registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if(result.getResultCode()==RESULT_OK){
+                    if(result.getData()!=null){
+                        String data=result.getData().getStringExtra("data");
+                        mEtAddress.setText(data);
+                    }
+                }
+            }
+    );
 }
---
+++ app/src/main/java/com/example/petbomi/SearchActivity.java
@@ -0,0 +1,39 @@
+package com.example.petbomi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        WebView webView=findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new BridgeInterface(),"Android");
+        webView.setWebViewClient((new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.loadUrl("javascript:sample2_execDaumPostcode()");
+            }
+        }));
+            webView.loadUrl("https://mapsearch-40c6d.web.app");
+    }
+
+    private class BridgeInterface {
+        @JavascriptInterface
+        public void processDATA(String data){
+            Intent intent=new Intent();
+            intent.putExtra("data",data);
+            setResult(RESULT_OK,intent);
+            finish();
+        }
+    }
+}
