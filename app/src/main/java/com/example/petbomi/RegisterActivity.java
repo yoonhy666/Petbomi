9cfe1fbfbc1fe574ec9c1241e1a61a33a25341ee
DESKTOP-193FPLI\윤혜은 <pih171260@gmail.com>
2022-09-30 15:26:37 +0900

    삭제 파일 반영

--- app/src/main/java/com/example/petbomi/RegisterActivity.java
+++ app/src/main/java/com/example/petbomi/RegisterActivity.java
@@ -1,11 +1,19 @@
 package com.example.petbomi;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -18,66 +26,42 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
 
 public class RegisterActivity extends AppCompatActivity {
-    private Spinner spinner;
+
+    private static final String TAG="RegisterActivity";
     private EditText mEtAddress;
-    private FirebaseAuth nFirebaseAuth; //파이어베이스 인증
-    private DatabaseReference nDatabaseRef;
-    private EditText eemail,epass,ename,enick,ephone,eage,egen,eaddr,eaddr2;
-    private Button regbuttn;
+    private FirebaseAuth mAuth;
+
+    Calendar calendar=Calendar.getInstance();
+    DatePickerDialog.OnDateSetListener datepicker=new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+            calendar.set(Calendar.YEAR,year);
+            calendar.set(Calendar.MONTH,month);
+            calendar.set(Calendar.DAY_OF_MONTH,day);
+            updateLabel();
+        }
+    };
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        nFirebaseAuth=FirebaseAuth.getInstance();
-        nDatabaseRef= FirebaseDatabase.getInstance().getReference();
-
-        eemail=findViewById(R.id.emailText);
-        epass=findViewById(R.id.passwordText);
-        ename=findViewById(R.id.name);
-        enick=findViewById(R.id.nickname);
-        ephone=findViewById(R.id.phnum);
-        eage=findViewById(R.id.age);
-        
-        eaddr=findViewById(R.id.et_address);
-        eaddr2=findViewById(R.id.et_address2);
 
 
-        regbuttn.setOnClickListener(new View.OnClickListener() {
+        EditText et_Date = (EditText) findViewById(R.id.birth);
+        et_Date.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                //회원가입 처리 시작
-                String stremail=eemail.getText().toString();
-                String strpass=epass.getText().toString();
-                String strname=ename.getText().toString();
-                String strnick=enick.getText().toString();
-                String strphone=ephone.getText().toString();
-                String strage=eage.getText().toString();
-                String strgen=spinner.getSelectedItem().toString();
-                String straddr=eaddr.getText().toString();
-                String straddr2=eaddr2.getText().toString();
-
-                //firebase auth 진행
-                nFirebaseAuth.createUserWithEmailAndPassword(stremail,strpass).addOnCompleteListener
-                        (RegisterActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if(task.isSuccessful()){
-                                    FirebaseUser firebaseuser=nFirebaseAuth.getCurrentUser();
-                                    UserAccount account=new UserAccount();
-                                    account.setEmailId(firebaseuser.getEmail());
-                                    account.setPassword(strpass);
-                                }
-                            }
-                        });
+            public void onClick(View v) {
+                new DatePickerDialog(RegisterActivity.this, datepicker, calendar.get(Calendar.YEAR),
+                        calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
-        spinner = (Spinner)findViewById(R.id.gender);
 
         mEtAddress=findViewById(R.id.et_address);
         mEtAddress.setFocusable(false);
@@ -89,7 +73,10 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.signUpButton).setOnClickListener(onClickListener);
+
     }
+
 
     private final ActivityResultLauncher<Intent> getSearchResult=registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -102,4 +89,67 @@ public class RegisterActivity extends AppCompatActivity {
                 }
             }
     );
+    private void updateLabel() {
+        String myFormat = "yyyy/MM/dd";    // 출력형식   2021/07/26
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
+
+        EditText et_date = (EditText) findViewById(R.id.birth);
+        et_date.setText(sdf.format(calendar.getTime()));
+    }
+    public void toStart(){
+        super.onStart();
+        FirebaseUser currentUser=mAuth.getCurrentUser();
+    }
+
+    View.OnClickListener onClickListener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.signUpButton:
+                    SignUp();
+
+                    break;
+            }
+
+        }
+    };
+    private void SignUp(){
+        String email=((EditText)findViewById(R.id.emailText)).getText().toString();
+        String password=((EditText)findViewById(R.id.passwordText)).getText().toString();
+        String name=((EditText)findViewById(R.id.name)).getText().toString();
+        String phone=((EditText)findViewById(R.id.phone)).getText().toString();
+        String nickname=((EditText)findViewById(R.id.nickname)).getText().toString();
+        String birth=((EditText)findViewById(R.id.birth)).getText().toString();
+        String petname=((EditText)findViewById(R.id.petname)).getText().toString();
+        RadioGroup petkind=((RadioGroup)findViewById(R.id.petkind));
+        RadioGroup petgender=((RadioGroup)findViewById(R.id.petgender));
+        RadioButton kindbtn = findViewById( petkind.getCheckedRadioButtonId() );
+        RadioButton genbtn = findViewById( petgender.getCheckedRadioButtonId() );
+        String kind=kindbtn.getText().toString().toUpperCase();
+        String gender=genbtn.getText().toString().toUpperCase();
+        String petage=((EditText)findViewById(R.id.petage)).getText().toString();
+        String address=((EditText)findViewById(R.id.et_address)).getText().toString();
+        String address2=((EditText)findViewById(R.id.et_address2)).getText().toString();
+        String etc=((EditText)findViewById(R.id.etc)).getText().toString();
+
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //ui
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                           //ui
+
+                        }
+                    }
+                });
+    }
 }
