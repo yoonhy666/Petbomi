43ab056b8bb9c61bb892705d3c5b43bbc2dac6f5
DESKTOP-193FPLI\윤혜은 <pih171260@gmail.com>
2022-11-01 11:38:26 +0900

    11.01(2)

---
+++ app/src/main/java/com/example/petbomi/LocationActivity.java
@@ -0,0 +1,15 @@
+package com.example.petbomi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+    }
+}
