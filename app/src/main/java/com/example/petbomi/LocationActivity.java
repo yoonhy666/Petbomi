8989394192b41003bce5fd6c9d0567e542d87486
DESKTOP-193FPLI\윤혜은 <pih171260@gmail.com>
2022-11-03 21:05:22 +0900

    카카오맵으로 변경

--- app/src/main/java/com/example/petbomi/LocationActivity.java
+++ app/src/main/java/com/example/petbomi/LocationActivity.java
@@ -3,6 +3,8 @@ package com.example.petbomi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import net.daum.mf.map.api.MapView;
 
 public class LocationActivity extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class LocationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
 
+        MapView mapView = new MapView(this);
+
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.mapView);
+        mapViewContainer.addView(mapView);
     }
 }
--- app/src/main/java/com/example/petbomi/MainActivity.java
+++ app/src/main/java/com/example/petbomi/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+
 import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
+
+
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +26,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getHashKey();
 
         Button button=(Button) findViewById(R.id.registerButton);
 
@@ -43,25 +46,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
-    private void getHashKey(){
-        PackageInfo packageInfo = null;
+    private void getAppKeyHash() {
         try {
-            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageInfo == null)
-            Log.e("KeyHash", "KeyHash:null");
-
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md;
+                md = MessageDigest.getInstance("SHA");
                 md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            } catch (NoSuchAlgorithmException e) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+                String something = new String(Base64.encode(md.digest(), 0));
+                Log.e("Hash key", something);
             }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            Log.e("name not found", e.toString());
         }
     }
 }
