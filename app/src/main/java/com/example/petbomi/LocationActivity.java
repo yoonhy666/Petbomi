d5450032be0fe9566456f8c52cce004833c02836
DESKTOP-193FPLI\윤혜은 <pih171260@gmail.com>
2022-11-10 19:43:28 +0900

    리스트 올리기 성공

--- app/src/main/java/com/example/petbomi/LocationActivity.java
+++ app/src/main/java/com/example/petbomi/LocationActivity.java
@@ -1,12 +1,21 @@
 package com.example.petbomi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
@@ -14,8 +23,15 @@ import net.daum.mf.map.api.MapView;
 
 import org.w3c.dom.Document;
 
-public class LocationActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class LocationActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<User> arrayList;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -129,6 +145,38 @@ public class LocationActivity extends AppCompatActivity {
         mapView.addPOIItem(marker8);
         mapView.addPOIItem(marker9);
         mapView.addPOIItem(marker10);
+
+        recyclerView = findViewById(R.id.recyclerView); // 아디 연결
+        recyclerView.setHasFixedSize(true); // 리사이클러뷰 기존성능 강화
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        arrayList = new ArrayList<>(); // User 객체를 담을 어레이 리스트 (어댑터쪽으로)
+
+        database = FirebaseDatabase.getInstance(); // 파이어베이스 데이터베이스 연동
+
+        databaseReference = database.getReference("User"); // DB 테이블 연결
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
+                arrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
+                    User user = snapshot.getValue(User.class); // 만들어뒀던 User 객체에 데이터를 담는다.
+                    arrayList.add(user); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
+                }
+                adapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // 디비를 가져오던중 에러 발생 시
+                Log.e("LocationSheet", String.valueOf(databaseError.toException())); // 에러문 출력
+            }
+        });
+
+
+        adapter = new UserAdapter(arrayList,this);
+        recyclerView.setAdapter(adapter); // 리사이클러뷰에 어댑터 연결
 
     }
 }
