24f6befbd29cab9929e002c7c33997cf5d3f038e
0219 <19qqor@naver.com>
2022-11-16 23:18:49 +0900

    날짜/시간 텍스트뷰 추가

--- app/src/main/java/com/example/petbomi/BookingActivity.java
+++ app/src/main/java/com/example/petbomi/BookingActivity.java
@@ -1,5 +1,6 @@
 package com.example.petbomi;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,8 +8,10 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class BookingActivity extends AppCompatActivity {
 
@@ -17,6 +20,14 @@ public class BookingActivity extends AppCompatActivity {
     private TextView bomi_name;
     private Intent intent;
     String name;
+
+    //달력
+    CalendarView calendar;
+    TextView tv_date;
+
+    //시간
+    TimePicker time_picker;
+    TextView tv_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +60,33 @@ public class BookingActivity extends AppCompatActivity {
         bomi_name = findViewById(R.id.bomi_name);
         bomi_name.setText(name);
 
+        //달력 날짜 가져오기
+        calendar = findViewById(R.id.calendar);
+        tv_date = findViewById(R.id.tv_date);
+
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int year, int month, int day) {
+                tv_date.setText(year + "년 " + (month + 1) + "월 " + day + "일 선택");
+            }
+
+        });
+
+        //시간 가져오기
+        time_picker = findViewById(R.id.time_picker);
+        tv_time = findViewById(R.id.tv_time);
+
+        time_picker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
+                if (hour > 12) {
+                    hour -= 12;
+                    tv_time.setText("오후 " + hour + "시 " + minute + "분 선택");
+                } else {
+                    tv_time.setText("오전 " + hour + "시 " + minute + "분 선택");
+                }
+            }
+        });
 
     }
 }
