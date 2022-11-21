fc0fc65a5e6993ef63315bfc8ec6c82ee4f8808b
0219 <19qqor@naver.com>
2022-11-21 19:26:49 +0900

    마이페이지 디자인 추가

--- app/src/main/java/com/example/petbomi/MyAddressActivity.java
+++ app/src/main/java/com/example/petbomi/MyAddressActivity.java
@@ -3,12 +3,23 @@ package com.example.petbomi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MyAddressActivity extends AppCompatActivity {
+    private ImageButton backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_address);
+
+        backbtn = findViewById(R.id.backbtn);
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
